db339edaffe28c59867eb1221b3092cb80cb7031
cybercrimineel <cybercrimineel@pm.me>
2019-12-19 15:43:43 +0100

    added pug filter for script

--- render.js
+++ render.js
@@ -1,14 +1,21 @@
-require('pug').renderFile(
-    './index.pug',
-    Object.assign(require('./state.json'), {
-        filters: {'style': function (text, options) {
-            return '<style>' + require('sass').renderSync(Object.assign(options, {
-                data: text,
-                indentedSyntax: true,
-                outputStyle: 'compressed'
-            })).css.toString() + '</style>';
-        }}
-    }),
+const state = require('./state.json');
+
+require('pug').renderFile('./index.pug', Object.assign(state, {filters: {
+    'script': function (text, options) {
+        delete options.filename;
+        return '<script>' + require('uglify-js').minify(
+            eval('`' + text + '`'),
+            options
+        ).code + '</script>';
+    },
+    'style': function (text, options) {
+        return '<style>' + require('sass').renderSync(Object.assign(options, {
+            data: text,
+            indentedSyntax: true,
+            outputStyle: 'compressed'
+        })).css.toString() + '</style>';
+    }
+}}),
     function (err, html) {
         if (err) throw err;
         process.stdout.write(html.replace(/\r?\n/g, ' '));
