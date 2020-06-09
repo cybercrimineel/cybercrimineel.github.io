c4d9df14ad43a3816b6b81ec6bf63f20ff4b87a5
cybercrimineel <cybercrimineel@pm.me>
2020-06-09 15:43:10 +0200

    may have fixed analytics

--- script.ts
+++ script.ts
@@ -1,15 +1,4 @@
-import { gtag, media, title } from './state.json';
-
-document.head.appendChild(Object.assign(document.createElement('script'), {
-    async: true,
-    src: `https://www.googletagmanager.com/gtag/js?id=${gtag}`
-}));
-
-(window => {
-    window.dataLayer = window.dataLayer || [];
-    window.dataLayer.push('js', new Date());
-    window.dataLayer.push('config', gtag);
-})(window as Window & typeof globalThis & { dataLayer: any[] });
+import { media, title } from './state.json';
 
 const skip = new URL(location.href).searchParams.get('_');
 
